813d7c719529eaef6eb79c802e05cae38c407364
Natasha1324 <165684802+Natasha1324@users.noreply.github.com>
2024-04-01 17:18:32 -1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_No._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string correctusername = "Keisha Herrera";
+            string correctpassword = "Keisha1234";
+            int attemps = 3;
+            while (attemps > 0) 
+            {
+                Console.Write("Input username: ");
+                string username = Console.ReadLine();
+                Console.Write("Input password: ");
+                string password = Console.ReadLine();
+
+                if (username == correctusername && password == correctpassword)
+                {
+                    Console.WriteLine("Login Successful");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect username/password. Please try again!");
+                    attemps--;
+                    Console.WriteLine($"You have {attemps} attempts left!");
+                }
+            }
+            if (attemps == 0)
+            {
+                Console.WriteLine("You have reached the maximum no. of attemps. Please try again later!");
+            }
+        }
+    }
+}
